81de19c20e7abc1d7feb6ded6e37e9946cd157dc
Saptarshi Mandal <saptarshicoding@gmail.com>
2022-07-10 21:20:44 +0530

    Some Changes

--- src/models/crypto.js
+++ src/models/crypto.js
@@ -13,38 +13,47 @@ const cryptoSchema=new mongoose.Schema({
     expense:{
         type: Boolean,
         required: true,
+        default: false
     },
     iscrypto:{
         type: Boolean,
+        required: true,
         default: false,
     },
     description:{
         type: String,
+        required: true,
         default: 'Just a note',
     },
     buyorsell:{
         type: Boolean,
         default: false,
+        required: true,
     },
     rate:{
         type: Number,
         default: 0,
+        required: true,
     },
     quantity:{
         type: Number,
         default: 0,
+        required: true,
     },
     amount:{
         type: Number,
         required: true,
+        default: 0
     },
     note:{
         type: String,
         default: 'Note Details',
+        required: true,
     },
     date:{
         type: Date,
         default: Date.now,
+        required: true,
     },
 });
 
